20f07eba4ea926206977a6f449a5de47069f87dd
Tatsuya Ishikawa <tatsuyai713@gmail.com>
2024-01-08 03:24:37 +0900

    Fix bug

--- src/rcl_like_wrapper.cpp
+++ src/rcl_like_wrapper.cpp
@@ -4,7 +4,9 @@
 
 namespace rcl_like_wrapper {
 
-static MessageTypes message_types;
+namespace {
+    MessageTypes message_types;
+}
 
 // Node
 int64_t node_create_node(int64_t domain_id) {
@@ -35,16 +37,19 @@ void node_stop_spin(int64_t node_ptr) {
 int64_t publisher_create_publisher(int64_t node_ptr, std::string message_type_name, std::string topic,
                                  dds::TopicQos& qos) {
   auto node = reinterpret_cast<Node *>(node_ptr);
+  // Creating a publisher with the specified message type, topic, and QoS.
   return reinterpret_cast<int64_t>(node->create_publisher(message_types.at(message_type_name), std::string("rt/") + topic, qos));
 }
 
 void publisher_publish_impl(int64_t publisher_ptr, void* message) {
   auto publisher = reinterpret_cast<Publisher *>(publisher_ptr);
+  // Publishing the message using the specified publisher.
   publisher->publish(message);
 }
 
 int32_t publisher_get_subscription_count(int64_t publisher_ptr) {
   auto publisher = reinterpret_cast<Publisher *>(publisher_ptr);
+  // Getting the subscription count associated with the publisher.
   return publisher->get_subscription_count();
 }
 
@@ -53,16 +58,23 @@ int64_t subscription_create_subscription(int64_t node_ptr, std::string message_t
     auto node = reinterpret_cast<Node*>(node_ptr);
     MessageType& message_type = message_types.at(message_type_name);
 
-    return reinterpret_cast<int64_t>(node->create_subscription(message_type, std::string("rt/") + topic, qos, callback));
+    // Creating a subscription with the specified message type, topic, QoS, and callback function.
+    auto subscription = node->create_subscription(message_type, std::string("rt/") + topic, qos, [callback](void* message_data) {
+        // Call the provided callback with the message data.
+        callback(message_data);
+    });
+
+    return reinterpret_cast<int64_t>(subscription);
 }
 
 int32_t subscription_get_publisher_count(int64_t subscription_ptr) {
   auto subscription = reinterpret_cast<Subscription *>(subscription_ptr);
+  // Getting the publisher count associated with the subscription.
   return subscription->get_publisher_count();
 }
 
 void rcl_like_wrapper_init(const MessageTypes &types) {
-  // Init MessageTypes
+  // Initializing MessageTypes.
   message_types = types;
 }
 
