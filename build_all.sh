adbf1fdef17bab0b344c92d4e47007b9deb6dc73
Tatsuya Ishikawa <tatsuyai713@gmail.com>
2024-01-22 02:52:31 +0900

    Add script

--- build_all.sh
+++ build_all.sh
@@ -1,9 +1,41 @@
 #!/bin/bash
 
-rm -rf build
+OPT=$1
+OPT_NUM=$#
+
+# clean
+if [ ! $OPT_NUM -ne 1 ]; then
+  if [ "clean" = $OPT ]; then
+    sudo rm -rf ./build
+    mkdir ./build
+    exit
+  fi
+fi
+
 mkdir build
 cd build
-cmake ..
-make
+
+INSTALL_PATH=/opt/fast-dds
+sudo mkdir -p /opt/fast-dds-libs
+
+cmake ..  -DCMAKE_BUILD_TYPE=Release \
+  -DCMAKE_SYSTEM_PREFIX_PATH=$INSTALL_PATH \
+  -DCMAKE_PREFIX_PATH=$INSTALL_PATH \
+  -DCMAKE_INSTALL_PREFIX=/opt/fast-dds-libs
+make -j4
 
 
+if [ ! $OPT_NUM -ne 1 ]; then
+	if [ "install" = $OPT ]; then
+                sudo make install
+                grep 'export LD_LIBRARY_PATH=/opt/fast-dds-libs/lib:$LD_LIBRARY_PATH' ~/.bashrc
+                if [ $? = 0 ]; then
+                        echo "LD_LIBRARY_PATH libs are already added"
+                else
+                        echo 'export LD_LIBRARY_PATH=/opt/fast-dds-libs/lib:$LD_LIBRARY_PATH' >> ~/.bashrc
+                        source ~/.bashrc
+                fi
+                sudo ldconfig
+	fi
+
+fi
