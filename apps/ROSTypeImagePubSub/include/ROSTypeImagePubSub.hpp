a581f06dfea49cbb674c408c0ea9fd82915d9b40
Ishikawa, Tatsuya (SGC) <tatsuya.a.ishikawa@sony.com>
2024-02-11 19:42:11 +0900

    Add PubSub sample

---
+++ apps/ROSTypeImagePubSub/include/ROSTypeImagePubSub.hpp
@@ -0,0 +1,40 @@
+#ifndef ROSTYPEIMAGEPUBLSUB_H_
+#define ROSTYPEIMAGEPUBLSUB_H_
+
+#include "rcl_like_wrapper.hpp"
+#include "sensor_msgs/msg/Image.h"
+#include "sensor_msgs/msg/ImagePubSubTypes.h"
+#include <functional>
+#include <memory>
+#include <string>
+#include <yaml-cpp/yaml.h>
+
+using namespace rcl_like_wrapper;
+class ROSTypeImagePubSub : public RCLWNode {
+public:
+    ROSTypeImagePubSub(uint16_t domain_number);
+    virtual ~ROSTypeImagePubSub();
+
+    // Override init and run methods from RCLWNode
+    bool init(const std::string& config_file_path) override;
+    // void run() override;
+
+    // Callback function to publish data
+    void callbackPublish(int test);
+
+    // Callback function to subscribe data
+    void callbackSubscribe(void* message);
+
+private:
+    std::string publish_topic_name_;
+    std::string subscribe_topic_name_;
+    uint16_t interval_ms_;
+    intptr_t publisher_ptr_;
+    intptr_t subscriber_ptr_;
+    intptr_t timer_ptr_;
+    std::function<void()> timer_callback_;
+    MessageTypes message_types_;
+    int counter_;
+};
+
+#endif /* ROSTYPEIMAGEPUBLSUB_H_ */
---
+++ apps/ROSTypeImagePubSub/src/ROSTypeData_main.cpp
@@ -0,0 +1,40 @@
+#include "ROSTypeImagePubSub.hpp"
+#include <iostream>
+#include <unistd.h>
+#include <limits.h>
+#include <string>
+
+int main(int argc, char **argv) {
+    std::cout << "Starting ROSTypeImagePubSub" << std::endl;
+
+    // Buffer to hold the path to the current executable
+    char buf[PATH_MAX] = {0};
+    std::string exePath, configPath;
+
+    // Attempt to read the symbolic link '/proc/self/exe' to find the path of the current executable
+    ssize_t count = readlink("/proc/self/exe", buf, sizeof(buf));
+    if (count != -1) {
+        exePath = std::string(buf, count); // Convert to string and assign
+        size_t pos = exePath.rfind('/'); // Find the last occurrence of '/'
+        if (pos != std::string::npos) {
+            configPath = exePath.substr(0, pos + 1); // Extract the path without executable name
+        }
+    } else {
+        std::cerr << "Failed to determine the path of the executable." << std::endl;
+        return 1;
+    }
+
+    // Initialize and run the ROS-like node
+    ROSTypeImagePubSub rcl_like_node(0);
+    configPath += "config/config.yaml"; // Append the relative path of the config file
+    std::cout << "Using config file at: " << configPath << std::endl;
+    
+    if (rcl_like_node.init(configPath)) {
+        rcl_like_node.spin();
+    } else {
+        std::cerr << "Failed to initialize the ROSTypeImagePubSub." << std::endl;
+        return 1;
+    }
+
+    return 0;
+}
