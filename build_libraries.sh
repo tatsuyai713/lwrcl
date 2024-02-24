365a283690aa1f2f05b5b4eb49e7d218e85e67c6
Tatsuya Ishikawa <tatsuyai713@gmail.com>
2024-02-25 00:57:45 +0900

    Update scripts

--- build_libraries.sh
+++ build_libraries.sh
@@ -15,15 +15,24 @@ fi
 cd libraries
 mkdir build
 cd build
-
 DDS_PATH=/opt/fast-dds
 INSTALL_PATH=/opt/fast-dds-libs
 sudo mkdir -p $INSTALL_PATH
 
-cmake ..  -DCMAKE_BUILD_TYPE=Debug \
+export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:$DDS_PATH/lib
+
+cmake ..  -DCMAKE_BUILD_TYPE=Release \
   -DCMAKE_SYSTEM_PREFIX_PATH=$DDS_PATH \
   -DCMAKE_PREFIX_PATH=$DDS_PATH \
-  -DCMAKE_INSTALL_PREFIX=$INSTALL_PATH
+  -Dfastcdr_DIR=$DDS_PATH/lib/cmake/fastcdr/ \
+  -Dfastrtps_DIR=$DDS_PATH/share/fastrtps/cmake/ \
+  -Dfoonathan_memory_DIR=$DDS_PATH/lib/foonathan_memory/cmake/ \
+  -Dtinyxml2_DIR=$DDS_PATH/lib/cmake/tinyxml2/ \
+  -DCMAKE_INSTALL_PREFIX=$INSTALL_PATH \
+  -DCMAKE_PREFIX_PATH=$DDS_PATH \
+  -DYAML_BUILD_SHARED_LIBS=ON \
+  -DYAML_CPP_INSTALL=ON
+
 make -j4
 
 if [ ! $OPT_NUM -ne 1 ]; then
